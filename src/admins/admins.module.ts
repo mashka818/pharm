03bfb63d0b503d590ec9b4b36d76bc4ba5e47bc5
Maria <mariavoronuk122@gmail.com>
2025-09-11 15:32:02 +0300

    fix: fixed email confirmation

--- src/admins/admins.module.ts
+++ src/admins/admins.module.ts
@@ -3,11 +3,12 @@ import { AdminsService } from './admins.service';
 import { AdminsController } from './admins.controller';
 import { PrismaService } from 'src/prisma.service';
 import { AuthModule } from 'src/auth/auth.module';
+import { CustomersModule } from 'src/customers/customers.module';
 
 @Module({
   controllers: [AdminsController],
   providers: [AdminsService, PrismaService],
   exports: [AdminsService],
-  imports: [forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule), CustomersModule],
 })
 export class AdminsModule {}
